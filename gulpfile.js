d75d4af97d9f68233c14628d594092f6e5eaff97
Tsuyoshi Iwanaga <Iwanaga.Tsuyoshi@trans-cosmos-dmi.co.jp>
2017-10-20 19:52:20 +0900

    add gulpfile modify sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var config = require('./config.js');
 
-gulp.task('default', funciton(){
-
+gulp.task('default', function(){
+  return gulp.src(config.assets + '/**/*.scss')
+  .pipe(sassGlob())
+  .pipe(sass({ outputStyle: 'expanded' }))
+  .pipe(gulp.dest(config.build));
 });
