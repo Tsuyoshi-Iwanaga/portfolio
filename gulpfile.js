b519ff26ac783e1684ea1fcc3abdd8821d6c375d
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2017-10-21 15:53:42 +0900

    add scss files

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var sassGlob = require('gulp-sass-glob');
 var config = require('./config.js');
 
 gulp.task('default', function(){
+});
+
+gulp.task('sass', function(){
   return gulp.src(config.assets + '/**/*.scss')
   .pipe(sassGlob())
   .pipe(sass({ outputStyle: 'expanded' }))
