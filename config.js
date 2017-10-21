e8676f0fe6fd67a3ea639805cb0ecc99b3eca4a7
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2017-10-22 01:29:57 +0900

    add sass files

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
   build: './component',
   assets: './component/assets',
-  scss: './component/assets/scss'
+  scss: './component/assets/scss',
+  targetBrowsers: ['last 2 version', 'iOS >= 8', 'Android >= 4']// '> 3%'などの指定も可能
 };
+
