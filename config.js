e02c56a41cc1dcf34ba4ae528b888f286a1349dc
Tsuyoshi Iwanaga <Iwanaga.Tsuyoshi@trans-cosmos-dmi.co.jp>
2017-10-24 19:56:09 +0900

    modify component -> template

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  build: './component',
-  assets: './component/assets',
-  scss: './component/assets/scss',
+  build: './template',
+  assets: './template/assets',
+  scss: './template/assets/scss',
   targetBrowsers: ['last 2 version', 'iOS >= 8', 'Android >= 4']// '> 3%'などの指定も可能
 };
 
