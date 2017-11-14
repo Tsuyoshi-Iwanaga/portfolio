e5028e7afe5f4f63ace69887659c99fa4e5d0624
Tsuyoshi Iwanaga <Iwanaga.Tsuyoshi@trans-cosmos-dmi.co.jp>
2017-11-14 17:39:38 +0900

    modify plugin sample

--- mydev/plugin_sample/plugin_sample.js
+++ mydev/plugin_sample/plugin_sample.js
@@ -1,79 +1,62 @@
-;(function(factory) {
-  'use strict';
-  if (typeof define === 'function' && define.amd) {
-      define(['jquery'], factory);
-  } else if (typeof exports !== 'undefined') {
-      module.exports = factory(require('jquery'));
-  } else {
-      factory(jQuery);
-  }
-
-}(function($){
+;(function($){
   "use strict";
   var Sample = window.Sample || {};
 
-  // construction
-  // -------------------------
+  Sample = (function() {
 
-  Sample = (function (element, options) {
+      function Sample(element, options) {
+          var t = this;
 
-      var t = this;
+          t.initials = {
+              move: '.move',
+              move_distance: 100,
+              wide: '.wide',
+              wide_width: 100,
+              expand: '.expand',
+              expand_target: '.expand-target',
+              expand_show_speed: 300,
+              expand_hide_speed: 300
+          };
+          t.settings = $.extend({}, t.initials, options);
 
-      t.initials = {
-          move: '.move',
-          move_distance: 100,
-          wide: '.wide',
-          wide_width: 100,
-          expand: '.expand',
-          expand_target: '.expand-target',
-          expand_show_speed: 300,
-          expand_hide_speed: 300
-      };
-      t.settings = $.extend({}, t.initials, options);
+          $(t.settings.expand_target, element).slideUp(0);
 
-      t.initEvents(element);
+          t.initEvents(element);
+          return false;
+      }
 
-      return t;
-  });
+      return Sample;
 
-  // EventBinding
-  // -------------------------
+  }());
 
   Sample.prototype.initEvents = function(element) {
       var t = this;
-
       $(t.settings.move, element).on( 'click', function() {
           t.move( $(this) );
           return false;
       });
-
       $(t.settings.wide, element).on( 'click', function() {
           t.wide( $(this) );
           return false;
       });
-
       $(t.settings.expand, element).on( 'click', function() {
           t.expand( $(this), $(t.settings.expand_target, element) );
           return false;
       });
-
   };
 
-  // detail process
-  // -------------------------
-
   Sample.prototype.move = function(element) {
-      var t = this,
+      var
+          t = this,
           l = element.css('left'),
           ll;
 
-      if(l == 'auto') {
-          l = 0
-      } else {
-          l = l.replace(/px/, '');
-      }
-      ll = parseInt(l, 10) + t.settings.move_distance;
-
+          if(l == 'auto') {
+              l = 0
+          } else {
+              l = l.replace(/px/, '');
+          }
+          ll = parseInt(l, 10) + t.settings.move_distance;
       element.css({
           position: 'relative',
           left: ll + 'px'
@@ -82,10 +65,10 @@
   };
 
   Sample.prototype.wide = function(element) {
-      var t = this,
+      var
+          t = this,
           w = element.innerWidth(),
           ww = element.innerWidth() + t.settings.wide_width;
-
       element.css({
           width: ww + 'px'
       });
@@ -95,7 +78,6 @@
   Sample.prototype.expand = function(element, target) {
       var t = this;
       var key = element.attr('class');
-
       if( key.match(/open/) ) {
           element.removeClass('sample-open');
           target.slideUp(t.settings.expand_hide_speed);
@@ -106,54 +88,52 @@
       return false;
   };
 
-  // local options for override
-  // -------------------------
+
 
   $.fn.Sample = function() {
-      var element = this,
+      var
+          element = this,
           options = arguments[0],
-          l = element.length,
+          args = Array.prototype.slice.call(arguments, 1),
           i;
-
-      for (i = 0; i < l; i++) {
+      for (i = 0; i < element.length; i++) {
           if (typeof options == 'object' || typeof options == 'undefined') {
               element[i].Sample = new Sample(element[i], options);
-          }else{
-              console.log('error:cannot run Sample Pulgin. option value is wrong.');
           }
       }
       return element;
 
   };
 
+
   // activate
   // -------------------------
 
   $(function(){
 
-      $('.js-sample-01').Sample();
+    $('.js-sample-01').Sample();
 
-      $('.js-sample-02').Sample({
-          move: '.sample-move',
-          move_distance: 50,
-          wide: '.sample-wide',
-          wide_width: 50,
-          expand: '.sample-expand',
-          expand_target: '.sample-expand-target',
-          expand_show_speed: 1000,
-          expand_hide_speed: 500
-      });
+    $('.js-sample-02').Sample({
+        move: '.sample-move',
+        move_distance: 50,
+        wide: '.sample-wide',
+        wide_width: 50,
+        expand: '.sample-expand',
+        expand_target: '.sample-expand-target',
+        expand_show_speed: 1000,
+        expand_hide_speed: 500
+    });
 
-      $('.js-sample-03').Sample({
-          move: '.sample-move',
-          move_distance: 200,
-          wide: '.sample-wide',
-          wide_width: 200,
-          expand: '.sample-expand',
-          expand_target: '.sample-expand-target',
-          expand_show_speed: 100,
-          expand_hide_speed: 50
-      });
+    $('.js-sample-03').Sample({
+        move: '.sample-move',
+        move_distance: 200,
+        wide: '.sample-wide',
+        wide_width: 200,
+        expand: '.sample-expand',
+        expand_target: '.sample-expand-target',
+        expand_show_speed: 100,
+        expand_hide_speed: 50
+    });
 
   });
-}));
+})(jQuery);
