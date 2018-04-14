e4da1f19ecb75ead6c72f3985777e9a59954c89e
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2018-04-15 00:41:48 +0900

    add contactForm

--- wp-content/themes/co-beat/index.php
+++ wp-content/themes/co-beat/index.php
@@ -157,12 +157,7 @@
   <div class="l-contentInner">
     <h2 class="c-sectionTitle">MAIL FORM</h2>
     <p class="mailForm_desc">お客様からのご相談、企業様からのお問い合せをお待ちしております。<br class="onlyPC">責任を持って折り返しご連絡させて頂きます。<br>※個人情報の取り扱いについて本画面にて弊社取得する個人情報は適切にお取り扱いし、<br class="onlyPC">第三者に提供または開示することはございません。<br class="onlyPC">※ただし、内容によっては返信を控えさせて頂くこともございますのでご了承くださいませ。</p>
-    <form action="#" class="mailForm_entry">
-      <input type="text" placeholder="NAME">
-      <input type="text" placeholder="E-MAIL">
-      <textarea placeholder="MESSAGE"></textarea>
-      <button type="submit">送信する</button>
-    </form>
+    <?php echo do_shortcode( '[contact-form-7 id="5" title="co-beat_Form" html_class="mailForm_entry"]' ); ?>
   </div>
 </section><!-- /.l-mailForm -->
 <?php get_footer(); ?>
