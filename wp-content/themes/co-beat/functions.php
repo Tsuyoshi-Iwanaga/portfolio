4bf193ebbc0699ccce49dbfec09944022e47c202
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2018-04-18 01:01:44 +0900

    modify default images

--- wp-content/themes/co-beat/functions.php
+++ wp-content/themes/co-beat/functions.php
@@ -15,10 +15,9 @@ add_filter('excerpt_more', 'new_excerpt_more');
 add_theme_support('post-thumbnails');
 // set_post_thumbnail_size(512, 295, true);
 // add_image_size('category_image', 512, 295, true);
-// add_image_size('single_image', 512, 295, true);
 function default_thumbnail( $post_id ) {
   $post_thumbnail = get_post_meta( $post_id, $key = '_thumbnail_id', $single = true );
-  $default_thumbnail_id = '19';//default catch image ID
+  $default_thumbnail_id = '23';//default catch image ID
   if ( !wp_is_post_revision( $post_id ) ) {
       if ( empty( $post_thumbnail ) ) {
           update_post_meta( $post_id, $meta_key = '_thumbnail_id', $meta_value = $default_thumbnail_id );
