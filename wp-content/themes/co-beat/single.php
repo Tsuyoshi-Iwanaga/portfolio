4f47ada84c4e95537e6257db017ed139ccf5ef78
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2018-04-24 02:19:25 +0900

    modify blog page kv

--- wp-content/themes/co-beat/single.php
+++ wp-content/themes/co-beat/single.php
@@ -7,20 +7,18 @@
 ?>
 
 <article class="l-contentSection -middleWidth">
-  <div class="l-contentInner">
-    <section>
-      <h2 class="c-sectionTitle p-blog_title"><?php the_title(); ?></h2>
-      <p class="p-blog_date"><?php the_time('Y/m/d'); ?></p>
-      <p class="p-blog_category">カテゴリー名&nbsp;:&nbsp;<?php the_category(' &gt; '); ?></p>
-      <p class="p-blog_img"><?php the_post_thumbnail('full'); ?></p>
-      <div class="p-blog_content"><?php the_content(); ?></div>
-    </section>
-
-    <div class="p-tagCloud">
-      <?php wp_tag_cloud(); ?>
+  <section>
+    <div class="l-contentInner">
+        <h2 class="c-sectionTitle p-blog_title"><?php the_title(); ?></h2>
+        <p class="p-blog_date"><?php the_time('Y/m/d'); ?></p>
+        <p class="p-blog_category">カテゴリー名&nbsp;:&nbsp;<?php the_category(' &gt; '); ?></p>
     </div>
-
-  </div>
+    <p class="p-blog_img"><?php the_post_thumbnail('full'); ?></p>
+    <div class="l-contentInner">
+        <div class="p-blog_content"><?php the_content(); ?></div>
+        <div class="p-tagCloud"><?php wp_tag_cloud(); ?></div>
+    </div>
+  </section>
 </article>
 
 <?php
